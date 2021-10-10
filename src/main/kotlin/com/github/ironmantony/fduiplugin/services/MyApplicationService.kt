b398f9d0bc4132adb2f8f9e065897a6843ba1888
GitHub Action <action@github.com>
2021-10-10 02:35:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/ironmantony/fduiplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ironmantony/fduiplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ironmantony.fduiplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ironmantony.fduiplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ironmantony/fduiplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ironmantony/fduiplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ironmantony.fduiplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ironmantony.fduiplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
